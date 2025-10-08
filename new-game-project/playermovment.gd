24e830fa97e68182dcfd5d1bd8c8835b146355b4
lostlp09 <mrnootnoot09@gmail.com>
2025-10-08 15:27:11 +0200

    added 1 magic comat up attack
    its spawns a block above you which kills enemys

--- new-game-project/playermovment.gd
+++ new-game-project/playermovment.gd
@@ -1,9 +1,12 @@
 extends CharacterBody2D
 const speed = 300
-var acceleartion= 1
+var acceleartion= 1.5
 var jumptimer = 0
 var jumpallowed = true
+var cooldown = false
+@onready var rigidbody = preload("res://rigid_body_2d.tscn")
 @onready var Camera = $"../Camera2D"
+
 func _physics_process(delta: float) -> void:
 	self.velocity.y += 30
 	self.velocity.x = 0
@@ -11,31 +14,52 @@ func _physics_process(delta: float) -> void:
 	
 
 	if Input.is_action_pressed("left"):
+		
 		self.velocity.x = speed * -1
 	
 	if Input.is_action_pressed("right"):
 		self.velocity.x = speed		
-	move_and_slide()
-	Camera.position = self.position
+	
+
+
+
+	if Input.is_action_just_pressed("shoot up"):
+		if cooldown == false:
+			cooldown = true	
+			var clone = rigidbody.instantiate()
+			
+			clone.visible = true
+			clone.position = Vector2(self.position.x + 300,self.position.y -300)
+			
+			self.get_parent().add_child(clone)
+			await get_tree().create_timer(1).timeout.connect(func test() -> void: cooldown = false )
+
+		
+		
+		
 	
 	if Input.is_action_pressed("jump"):
-		
-	 
-		
-		if jumptimer < 1 and jumpallowed:
+	
+	
+	
+		if jumptimer < 0.3 and jumpallowed:
 
-			jumptimer += 0.1
-			acceleartion += 0.1
+			jumptimer += delta
+			acceleartion += 1.5* delta
 			self.velocity.y = speed * -1 * acceleartion
-		
 	elif not is_on_floor():
-		jumptimer = 1		
+		jumptimer = 1
+		
 	if is_on_floor():
 		jumpallowed = true
-		acceleartion =1
+		acceleartion =1.5
 		jumptimer = 0
 	if not is_on_floor() and jumptimer == 0:
 		jumpallowed = false
+	
+	
+	move_and_slide()
+	Camera.position = self.position
 		
 		
 	
